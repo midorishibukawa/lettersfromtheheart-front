e304c015fe5846f7b68d635c025b63fbf192f9d5
FShibukawa <felipeshibukawa@gmail.com>
2023-07-06 20:20:22 -0300

    correções na integração de criação de cartas

--- createLetter.js
+++ createLetter.js
@@ -1,20 +1,23 @@
-
-onload = () =>{
-    const submit = document.querySelector("#submitButton")
-    const textArea = document.querySelector("#letter")
-    submit.addEventListener("click", async ()=>{
-        const user = localStorage.getItem("user")
-        const letter = {
-            from: user,
-            text: textArea.value
-        }
-        const req = {
-            method: "POST",
-             body: JSON.stringify(letter),
-              headers:{"Content-Type": "application/json"}
-            }
-            const res = await fetch("http://localhost:3000/letter", req)
-            const data = await res.json()
-            window.location.replace("/lettersfromtheheart-front/showLetter.html?id="+data.id)
-    })
+const user = localStorage.getItem("user")
+const submit = document.querySelector("#submitButton")
+const textArea = document.querySelector("#letter")
+if (!user) {
+    window.location.replace("/login.html")
 }
+
+submit.addEventListener("click", async ($event)=>{
+    $event.preventDefault()
+    const letter = {
+        from: user,
+        text: textArea.value
+    }
+    const req = {
+        method: "POST",
+            body: JSON.stringify(letter),
+            headers:{"Content-Type": "application/json"}
+        }
+        const res = await fetch("http://localhost:3000/letter", req)
+        const data = await res.json()
+        window.location.replace("/showLetter.html?id="+data.id)
+        
+})
